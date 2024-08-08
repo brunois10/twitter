bd5f278c8c6e07a8e6e1e5962dc161cea90a0126
Bruno Guimarães <brunogdepaula@hotmail.com>
2024-08-08 04:12:54 -0300

    feat: add settings for pythonanywhere

--- twitter/settings.py
+++ twitter/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = "twitter.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "twitter", "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
